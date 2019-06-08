8bd30a44300cee3361ebb7d12ad4c91aa91b2252
mr-and <souhei37@gmail.ru>
2019-06-09 00:14:44 +0300

    08.06.19

--- 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1821/Solution.java
@@ -4,8 +4,39 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        String filename = args[0];
+        FileInputStream iStream = new FileInputStream("D:\\text.txt");
+
+        Map<Integer, Integer> map = new TreeMap<>();
+
+        while (iStream.available() > 0) {
+
+            int data = iStream.read();
+
+            if (map.containsKey(data)) {
+                map.put(data, map.get(data) + 1);
+            }
+            else {
+                map.put(data, 1);
+            }
+
+        }
+        for (Map.Entry<Integer, Integer> pair : map.entrySet()) {
+
+            int key = pair.getKey();
+            int value = pair.getValue();
+
+            System.out.println((char) key + " " + value);
+        }
+
+        iStream.close();
     }
 }
